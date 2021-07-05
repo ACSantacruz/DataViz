a46a55f099b077e0b0f3585b580aea71ee41438a
Coco <70581817+CocoCosmos@users.noreply.github.com>
2021-07-05 19:19:10 +0200

    heading

--- main.js
+++ main.js
@@ -14,6 +14,19 @@ OriginPop
 DestinationPop
 */
 function parseFile() {
-    const data = d3.tsvParse(await FileAttachment("flight_edges.tsv").text());
+    const data = d3.tsvParse(await FileAttachment("flight_edges.tsv").text(), d =>({
+        ORIG: +d["ORIG"],
+        DEST: +d["DEST"],
+        Origin: +d["Origin"],
+        Destination: +d["Destination"],
+        Passengers: +d["Passengers"],
+        SeatsAvailable: +d["SeatsAvailable"],
+        FlightsBTWN: +d["FlightsBTWN"],
+        DistanceMile: +d["DistanceMile"],	
+        DateYYYYMM: +d["DateYYYYMM"],
+        OriginPop: +d["OriginPop"],
+        DestinationPop: +d["DestinationPop"]
+    }));
         
 }
+console.log(data);
