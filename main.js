4c86ad979d8ecbb2a3e206a0a5ea69b7b9894f04
Coco <70581817+CocoCosmos@users.noreply.github.com>
2021-07-05 11:56:39 +0200

    Moar files, started on parsing

---
+++ main.js
@@ -0,0 +1,23 @@
+//DATA
+/*
+Columns:
+ORG 
+DES 
+Origin, ST
+Destination, ST
+Passengers
+SeatsAvailable 
+FlightsBTWN 
+DistanceMile 
+DateYYYYMM 
+OriginPop 
+DestinationPop
+*/
+data = {
+    const data = await FileAttachment("flight_edges.tsv").tsv({typed: true});
+    return 
+    });
+}
+
+
+
