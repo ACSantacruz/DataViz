71f59d3e24850c5bee4daeabd99d02f1ddc3949f
Coco <70581817+CocoCosmos@users.noreply.github.com>
2021-07-05 19:12:26 +0200

    Appended header to tsv

--- main.js
+++ main.js
@@ -13,33 +13,7 @@ DateYYYYMM
 OriginPop 
 DestinationPop
 */
-
-const data = d3.tsvParseRows("https://media.githubusercontent.com/media/CornelieFalcon/DataViz/main/flight_edges.tsv?token=AQ2P4OK5ZK4FCJQFHJ5GQILA4LUAS".text(), (d, i) => {
-    return {
-        ORIG = d[0],
-        DEST = d[1],
-        origin = d[2],
-        Destination = d[3], 
-        Passengers = d[4],
-        SeatsAvailable = d[5],
-        FlightsBTWN = d[6],
-        Distance = d[7],
-        DateYYYYMM = d[8],
-        ORIGPop = d[9],
-        DESTPop =d[10]
-    };
-});
-    
-    /*
-    return Object.assign(
-        data.map(d =>({
-            
-        }))*/
-    
-
-//const [dataValues] = d3.values(data);
-//const keys = Object.keys(dataValues)
-
-//reduceFn = data => d3.sum(data, d => d[""])
-
-//d3.rollups(data, v => v. )
+function parseFile() {
+    const data = d3.tsvParse(await FileAttachment("flight_edges.tsv").text());
+        
+}
